6d0921d99990bc7558e6f881b96720439316f789
yurgas <yurarsit@gmail.com>
2021-03-16 12:30:39 +0300

    Add outputs description

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
 output "vpc_id" {
+  description = "Spoke VPC id"
   value = google_compute_network.vpc.id
 }
 
 output "subnet_id" {
+  description = "Spoke subnet id"
   value = google_compute_subnetwork.subnet.id
 }
